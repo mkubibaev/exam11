78c2a393b7aa592c29173eaa410c30f67378ff7a
mkubibaev <mkubibaev@gmail.com>
2019-04-27 12:31:29 +0600

    #1 Добавлен auth middleware

--- api/models/User.js
+++ api/models/User.js
@@ -29,6 +29,10 @@ const UserSchema = new Schema({
         type: String,
         required: true
     },
+    phone: {
+        type: String,
+        required: true,
+    },
     token: {
         type: String,
         required: true
